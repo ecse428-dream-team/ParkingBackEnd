c92c1b6153228b0db650bfaa16e0177c0287852a
An Khang <an.k.chau@mail.mcgill.ca>
2019-01-30 19:54:37 -0500

    initial commit of backend and springboot set up

---
+++ src/main/java/ca/mcgill/ecse428/parkingsystem/ParkingSystemApplication.java
@@ -0,0 +1,22 @@
+package ca.mcgill.ecse428.parkingsystem;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@SpringBootApplication
+public class ParkingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSystemApplication.class, args);
+	}
+	
+	  @RequestMapping("/")
+	  public String greeting(){
+	    return "Hello world!";
+	  }
+}
+
---
+++ src/test/java/ca/mcgill/ecse428/parkingsystem/ParkingSystemApplicationTests.java
@@ -0,0 +1,17 @@
+package ca.mcgill.ecse428.parkingsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParkingSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
