446d034e6fe6468a726c31c394108af08fe120fe
roger.zhang@mail.mcgill.ca <roger.zhang@mail.mcgill.ca>
2019-01-31 19:48:28 -0500

    Modified usercontroller and user repository

--- src/main/java/ca/mcgill/ecse428/parkingsystem/controller/UserController.java
+++ src/main/java/ca/mcgill/ecse428/parkingsystem/controller/UserController.java
@@ -1,9 +1,13 @@
 package ca.mcgill.ecse428.parkingsystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.mcgill.ecse428.parkingsystem.model.User;
 import ca.mcgill.ecse428.parkingsystem.repository.UserRepository;
 
 @RestController
@@ -12,5 +16,15 @@ public class UserController {
 	
 	@Autowired
 	UserRepository repository;
+	
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+	
+	@PostMapping(consumes = "application/json", produces = "application/json")
+	public User createUser(@RequestBody User user) {
+	    return repository.createUser(user);
+	}
 
 }
--- src/main/java/ca/mcgill/ecse428/parkingsystem/repository/UserRepository.java
+++ src/main/java/ca/mcgill/ecse428/parkingsystem/repository/UserRepository.java
@@ -1,14 +1,23 @@
 package ca.mcgill.ecse428.parkingsystem.repository;
 
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import ca.mcgill.ecse428.parkingsystem.model.User;
 
 @Repository
 public class UserRepository {
 	
 	@Autowired
 	EntityManager entityManager;
+	
+	@Transactional
+	public User createUser(User user) {
+		entityManager.persist(user);
+		return user;
+	}
 
 }
