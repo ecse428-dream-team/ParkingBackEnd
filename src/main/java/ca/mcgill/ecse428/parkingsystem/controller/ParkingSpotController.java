c3cbe96698ba7f9edf70653f02f9f874b3334dea
Shayne Leitman <sleitman17@gmail.com>
2019-02-08 15:58:31 -0500

    Added methods to search by street number

--- src/main/java/ca/mcgill/ecse428/parkingsystem/controller/ParkingSpotController.java
+++ src/main/java/ca/mcgill/ecse428/parkingsystem/controller/ParkingSpotController.java
@@ -1,15 +1,20 @@
 package ca.mcgill.ecse428.parkingsystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse428.parkingsystem.model.ParkingSpot;
+import ca.mcgill.ecse428.parkingsystem.model.User;
 import ca.mcgill.ecse428.parkingsystem.repository.ParkingSpotRepository;
 
 @RestController
@@ -28,4 +33,14 @@ public class ParkingSpotController {
 	public ParkingSpot addParkingSpot(@RequestBody ParkingSpot ps) {
 	    return repository.addParkingSpot(ps);
 	}
+	
+//Make searches to create lists of parking spots with the same: Street name, street number, postal code,
+
+	@RequestMapping(value = "/streetnumber/{streetNum}", method = RequestMethod.GET)
+    public List<ParkingSpot> getSpotByStreetNumber(
+            @PathVariable("streetNum") int StreetNum) {
+        return repository.getSpotByStreetNumber(StreetNum);
+    }
+	
+	
 }
--- src/main/java/ca/mcgill/ecse428/parkingsystem/repository/ParkingSpotRepository.java
+++ src/main/java/ca/mcgill/ecse428/parkingsystem/repository/ParkingSpotRepository.java
@@ -1,12 +1,16 @@
 package ca.mcgill.ecse428.parkingsystem.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import ca.mcgill.ecse428.parkingsystem.model.ParkingSpot;
+import ca.mcgill.ecse428.parkingsystem.model.User;
 
 @Repository
 public class ParkingSpotRepository {
@@ -19,5 +23,18 @@ public class ParkingSpotRepository {
 		entityManager.persist(ps);
 		return ps;
 	}
+	
+	@Transactional
+    public List<ParkingSpot> getSpotByStreetNumber(int number) {
+	    TypedQuery<ParkingSpot> query1 = entityManager
+	    .createQuery("SELECT s FROM ParkingSpot s WHERE s.street_Number = :Streetnumber", ParkingSpot.class);
+        query1.setParameter("Streetnumber", number);
+	    List<ParkingSpot> spots = query1.getResultList();
+	    return spots;
+    }
+	
+	
+	
+	
 
 }
