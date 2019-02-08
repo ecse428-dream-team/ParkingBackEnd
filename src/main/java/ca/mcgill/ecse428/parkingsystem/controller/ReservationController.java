eaa27f3257e0bd6117cca8199dd5c969a773f3ff
Shayne Leitman <sleitman17@gmail.com>
2019-02-08 14:59:32 -0500

    Added reservation controller and repository classes

---
+++ src/main/java/ca/mcgill/ecse428/parkingsystem/controller/ReservationController.java
@@ -0,0 +1,22 @@
+package ca.mcgill.ecse428.parkingsystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import ca.mcgill.ecse428.parkingsystem.model.User;
+import ca.mcgill.ecse428.parkingsystem.repository.UserRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservation")
+public class ReservationController {
+
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/ca/mcgill/ecse428/parkingsystem/repository/ReservationRepository.java
@@ -0,0 +1,20 @@
+package ca.mcgill.ecse428.parkingsystem.repository;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import ca.mcgill.ecse428.parkingsystem.model.User;
+
+import java.util.List;
+
+@Repository
+public class ReservationRepository {
+
+	
+	
+	
+	
+}
