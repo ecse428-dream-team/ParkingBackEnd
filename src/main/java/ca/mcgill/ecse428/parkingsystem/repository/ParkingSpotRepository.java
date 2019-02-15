4d9a6abd7673e9b8388740d275edaffd55e42299
An Khang <an.k.chau@mail.mcgill.ca>
2019-02-14 22:59:22 -0500

    Get request for all and specific parkingspot implemented

--- src/main/java/ca/mcgill/ecse428/parkingsystem/repository/ParkingSpotRepository.java
+++ src/main/java/ca/mcgill/ecse428/parkingsystem/repository/ParkingSpotRepository.java
@@ -1,11 +1,20 @@
 package ca.mcgill.ecse428.parkingsystem.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import ca.mcgill.ecse428.parkingsystem.model.Admin;
 import ca.mcgill.ecse428.parkingsystem.model.ParkingSpot;
 
 @Repository
@@ -19,5 +28,18 @@ public class ParkingSpotRepository {
 		entityManager.persist(ps);
 		return ps;
 	}
+	
+	@Transactional
+	public ParkingSpot getParkingSpot(String id)
+	{
+		ParkingSpot foundParkingSpot = entityManager.find(ParkingSpot.class, id);
+		return foundParkingSpot;
+	}
 
+	@Transactional
+	public List<ParkingSpot> getParkingSpots(){
+		List<ParkingSpot> ParkingSpots = new ArrayList<ParkingSpot>(); 
+		ParkingSpots = entityManager.createQuery("SELECT a FROM ParkingSpot a").getResultList();
+		return ParkingSpots;
+	}
 }
