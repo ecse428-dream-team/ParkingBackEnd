632a1cd228b23b18fa07a69ec8b9215307e78e85
An Khang <an.k.chau@mail.mcgill.ca>
2019-02-14 22:53:42 -0500

    Get request for all and specific user implemented

--- src/main/java/ca/mcgill/ecse428/parkingsystem/repository/UserRepository.java
+++ src/main/java/ca/mcgill/ecse428/parkingsystem/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package ca.mcgill.ecse428.parkingsystem.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import ca.mcgill.ecse428.parkingsystem.model.Admin;
 import ca.mcgill.ecse428.parkingsystem.model.User;
 
 @Repository
@@ -20,4 +24,17 @@ public class UserRepository {
 		return user;
 	}
 
+	@Transactional
+	public User getUser(String id)
+	{
+		User foundUser = entityManager.find(User.class, id);
+		return foundUser;
+	}
+
+	@Transactional
+	public List<User> getUsers(){
+		List<User> users = new ArrayList<User>(); 
+		users = entityManager.createQuery("SELECT a FROM User a").getResultList();
+		return users;
+	}
 }
