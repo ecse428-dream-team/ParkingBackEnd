904b07db8358e9ec6b4e294d36be5c9ad1c66e34
An Khang <an.k.chau@mail.mcgill.ca>
2019-02-14 22:44:44 -0500

    Get request for all admin implemented

--- src/main/java/ca/mcgill/ecse428/parkingsystem/repository/AdminRepository.java
+++ src/main/java/ca/mcgill/ecse428/parkingsystem/repository/AdminRepository.java
@@ -1,5 +1,8 @@
 package ca.mcgill.ecse428.parkingsystem.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
@@ -26,5 +29,11 @@ public class AdminRepository {
 		Admin foundAdmin = entityManager.find(Admin.class, id);
 		return foundAdmin;
 	}
-	
+
+	@Transactional
+	public List<Admin> getAdmins(){
+		List<Admin> admins = new ArrayList<Admin>(); 
+		admins = entityManager.createQuery("SELECT a FROM Admin a").getResultList();
+		return admins;
+	}
 }
