bdb95d96b64261dbc60d818066e11a2f7f3e54b7
An Khang <an.k.chau@mail.mcgill.ca>
2019-02-26 22:22:04 -0500

    corrections for partial search

--- src/main/java/ca/mcgill/ecse428/parkingsystem/controller/ParkingSpotController.java
+++ src/main/java/ca/mcgill/ecse428/parkingsystem/controller/ParkingSpotController.java
@@ -25,14 +25,14 @@ public class ParkingSpotController {
 	
 	@GetMapping(path = "/id/{id}", produces = { MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<ParkingSpot> getParkingSpot(@PathVariable String id) {
-		ParkingSpot ParkingSpotFound = repository.getParkingSpot(id);
-		return new ResponseEntity<ParkingSpot>(ParkingSpotFound, null, HttpStatus.OK);
+		ParkingSpot parkingSpotFound = repository.getParkingSpot(id);
+		return new ResponseEntity<ParkingSpot>(parkingSpotFound, null, HttpStatus.OK);
 	}
 
 	@GetMapping(path = "/all", produces = { MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<List<ParkingSpot>> getParkingSpots() {
-		List<ParkingSpot> ParkingSpots = repository.getParkingSpots();
-		return new ResponseEntity<List<ParkingSpot>>(ParkingSpots, null, HttpStatus.OK);
+		List<ParkingSpot> parkingSpots = repository.getParkingSpots();
+		return new ResponseEntity<List<ParkingSpot>>(parkingSpots, null, HttpStatus.OK);
 	}
 	
 	@PostMapping(consumes = "application/json", produces = "application/json")
@@ -40,26 +40,44 @@ public class ParkingSpotController {
 	    return repository.addParkingSpot(ps);
 	}
 	
-	
-	
 	// All methods below here pertains to the newly added repository methods for general search and advanced searches
-	@GetMapping(path = "/general", produces = { MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<List<ParkingSpot>> generalSearch() {
-		return new ResponseEntity<List<ParkingSpot>>(repository.generalSearch(), null, HttpStatus.OK);
+	@GetMapping(path = "/partialSearch/id/{id}", produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<List<ParkingSpot>> partialSearchById(@PathVariable String id) {
+		List<ParkingSpot> parkingSpots = repository.partialSearchById(id);
+		return new ResponseEntity<List<ParkingSpot>>(parkingSpots, null, HttpStatus.OK);
 	}
 	
-	@GetMapping(path = "/advance/search1/{field}/{value}", produces = { MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<List<ParkingSpot>> advancedStringFieldSearch(@PathVariable String field, @PathVariable String value) {
-		return new ResponseEntity<List<ParkingSpot>>(repository.advancedStringFieldSearch(field, value), null, HttpStatus.OK);
+	@GetMapping(path = "/partialSearch/streetName/{name}", produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<List<ParkingSpot>> partialSearchByStreetName(@PathVariable String name) {
+		List<ParkingSpot> parkingSpots = repository.partialSearchByStreetName(name);
+		return new ResponseEntity<List<ParkingSpot>>(parkingSpots, null, HttpStatus.OK);
 	}
 	
-	@GetMapping(path = "/advance/search2/{field}/{value}", produces = { MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<List<ParkingSpot>> advancedFloatFieldSearch(@PathVariable String field, @PathVariable float value) {
-		return new ResponseEntity<List<ParkingSpot>>(repository.advancedFloatFieldSearch(field, value), null, HttpStatus.OK);
+	@GetMapping(path = "/partialSearch/postalCode/{code}", produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<List<ParkingSpot>> partialSearchByPostalCode(@PathVariable String code) {
+		List<ParkingSpot> parkingSpots = repository.partialSearchByPostalCode(code);
+		return new ResponseEntity<List<ParkingSpot>>(parkingSpots, null, HttpStatus.OK);
 	}
 	
-	@GetMapping(path = "/advance/search3/{value}", produces = { MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<List<ParkingSpot>> advancedStringFieldSearch(@PathVariable int value) {
-		return new ResponseEntity<List<ParkingSpot>>(repository.advancedSearchByStreetNum(value), null, HttpStatus.OK);
+//	@GetMapping(path = "/advance/search2/{field}/{value}", produces = { MediaType.APPLICATION_JSON_VALUE })
+//	public ResponseEntity<List<ParkingSpot>> advancedFloatFieldSearch(@PathVariable String field, @PathVariable float value) {
+//		return new ResponseEntity<List<ParkingSpot>>(repository.advancedFloatFieldSearch(field, value), null, HttpStatus.OK);
+//	}
+	
+	@GetMapping(path = "/partialSearch/underPrice/{price}", produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<List<ParkingSpot>> partialSearchByPrice(@PathVariable float price) {
+		List<ParkingSpot> parkingSpots = repository.partialSearchByUnderPrice(price);
+		return new ResponseEntity<List<ParkingSpot>>(parkingSpots, null, HttpStatus.OK);
 	}
+	
+	@GetMapping(path = "/partialSearch/overRating/{rating}", produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<List<ParkingSpot>> partialSearchbyOverRating(@PathVariable float rating) {
+		List<ParkingSpot> parkingSpots = repository.partialSearchByOverRating(rating);
+		return new ResponseEntity<List<ParkingSpot>>(parkingSpots, null, HttpStatus.OK);
+	}
+	
+//	@GetMapping(path = "/advance/search3/{value}", produces = { MediaType.APPLICATION_JSON_VALUE })
+//	public ResponseEntity<List<ParkingSpot>> advancedStringFieldSearch(@PathVariable int value) {
+//		return new ResponseEntity<List<ParkingSpot>>(repository.advancedSearchByStreetNum(value), null, HttpStatus.OK);
+//	}
 }
